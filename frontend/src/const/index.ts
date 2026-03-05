4b5e0bc5fad5828ceb7a226430a7b43ed17fbfd3
deli <yjx020714@gmail.com>
2026-03-05 17:01:47 +0800

    fix

--- frontend/src/const/index.ts
+++ frontend/src/const/index.ts
@@ -11,4 +11,4 @@ export const ZEROBANK_ADDRESS = "0xB848cdFaAD5A9C890eEE230c42805FA49Aaa8227";
 
 export const API_URL = import.meta.env.DEV
   ? "http://localhost:3000"
-  : "https://api.zerobank.com";
+  : "https://api.zerobank.io";
